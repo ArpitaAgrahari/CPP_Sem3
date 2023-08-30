7dd69d4f9dc06105c6b5b7d7bda8a3060b263e63
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-08-30 02:10:35 -0700

    Add files via upload

---
+++ Self_Tutorial/01_Basics.cpp
@@ -0,0 +1,41 @@
+//01_Basics
+/* 
+    - Low Level Programming Language
+    - Speed of execution
+    - OOP
+ */
+// Keywords - Words which hold a special meaning to the compiler.
+// Variables - Name given to a memory space.
+// Functions - A sequence of instructions that does a specific task.
+
+//Statically Typed                              Dynamically Typed
+//  C++, Java                                       Python
+
+// # -> Preprocessor directive
+// include -> keyword
+// iostream -> header file
+// namespace -> logical group
+
+#include <iostream>
+// int(here) -> return type
+// main() -> Function Name
+int main()
+{
+    std::cout<<"\033c";
+    std::cout << "Arpita" <<std::endl<<"Agrahari" <<std::endl;
+    return 0;
+}
+//02_Comments
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Single Line Comment
+    int i = 5;
+    /*
+        sadjhfoje
+        sdkjfhosd
+        sigosrf
+    */
+    return 0;
+}
---
+++ Self_Tutorial/02_Comments.cpp
@@ -0,0 +1,14 @@
+//02_Comments
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Single Line Comment
+    int i = 5;
+    /*
+        sadjhfoje
+        sdkjfhosd
+        sigosrf
+    */
+    return 0;
+}
