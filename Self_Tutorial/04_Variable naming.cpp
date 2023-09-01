5ed92bf788810655e6b31b6b1d8b0cd7abaf5251
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-09-01 05:41:58 -0700

    Add files via upload

---
+++ Self_Tutorial/04_Variable naming.cpp
@@ -0,0 +1,37 @@
+//04_Variable naming
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+        Allowed Characters: 
+        Lower Case
+        Upper Case
+        Digits 0 to 9
+        Underscore
+    */
+
+   /* 
+        Not Allowed:
+        Cannot begin with a digit
+        Should not be a keyword
+    */
+   int return1 = 5;
+   int Return1 = 15;
+   int a, X1, X_12;
+
+   /* 
+        Conventions : 
+        Camel Case(name, currentYear, playerName)
+            If one word variable then all small
+            If more than one word variable then starting letter of each word is capital(except First).
+        
+        Snake Case(name, currnet_year, player_name)
+            Seperate different words using underscore
+
+        Constants(MAX_AGE, PI)
+            All Capital
+        
+    */
+    return 0;
+}
