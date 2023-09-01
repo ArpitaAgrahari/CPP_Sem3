81866b15b39d555addbd763845febecc1f737b48
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-09-01 05:41:37 -0700

    Add files via upload

---
+++ Self_Tutorial/03_Variable.cpp
@@ -0,0 +1,11 @@
+//03_Variables
+//Variables serve the purpose of accessing memory cells and modifying the data at the memory.
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\033c";
+    int x = 5;
+    cout<<"x = "<<x<<endl;
+    return 0;
+}
