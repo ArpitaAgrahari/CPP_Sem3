d64701b4b004d95b3cf62cfe98757e7644666d5d
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-09-04 10:50:37 -0700

    Add files via upload

---
+++ Self_Tutorial/05_data types.cpp
@@ -0,0 +1,13 @@
+//05_data types
+/* 
+    Primitive :
+
+    Integer : short, int, long, long, unsgined short...
+    character type : char, unsigned char
+    Unicode Character Type: uint_32, char8_t, char16_t
+    Float : float, double, long double
+    Other : bool, void
+
+    Non Primitive :
+    Arrays, Pointers, class, structures, string, vectors
+ */
